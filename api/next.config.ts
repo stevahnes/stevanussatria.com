229f8e5a400690a36ccdfa4054f22f799acffbb5
Stevanus Satria <stevanus.satria@gmail.com>
2026-03-04 23:52:18 +0800

    feat: move from Langbase to Vercel AI SDK

--- api/next.config.ts
+++ api/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/app/chat": ["./src/context.ts"],
+  },
   async headers() {
     const frontendUrl =
       process.env.NEXT_PUBLIC_ALLOWED_ORIGIN ||
@@ -27,30 +30,6 @@ const nextConfig: NextConfig = {
             key: "Access-Control-Expose-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, lb-thread-id",
-          },
-        ],
-      },
-      {
-        source: "/thread/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: frontendUrl,
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          {
-            key: "Access-Control-Expose-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
